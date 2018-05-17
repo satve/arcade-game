50a2d80e01d6238dd5b00da6d9c6a3676f220e02
satveer <SD12@mitt.ca>
2018-05-17 14:39:25 -0500

    concate gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
+var concat = require('gulp-concat');
 
 gulp.task('css', () => {
 	gulp.src('./src/css/**/*.css')
@@ -20,9 +21,10 @@ gulp.task('images', () => {
 });
 
 gulp.task('js', () => {
-    gulp.src('./src/js/**/*.js') 
+    gulp.src(['./src/app.js', './src/engine.js', './src/resources.js']) 
   .pipe(babel({
         presets: ['env']
       }))
+      .pipe(concat('main.js'))
       .pipe(gulp.dest('./dist/js/'))
     });
