f44b422ae31401179b09edd2f57b373c502f02f8
satveer <SD12@mitt.ca>
2018-05-17 13:39:05 -0500

    autoprefix css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-
-});
+gulp.task('css', () =>
+	gulp.src('./src/css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+);
