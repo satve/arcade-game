7ad04372d941293857fa9c5ab6f06bceb894458e
satveer <SD12@mitt.ca>
2018-05-17 14:11:42 -0500

    covert ES6 to ES5

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,27 @@ var gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
-gulp.task('css', () =>
+gulp.task('css', () => {
 	gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer({
             browsers: ['last 4 versions'],
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./dist/css/'))
-);
+});
 
-gulp.task('images', () =>
+gulp.task('images', () => {
     gulp.src('src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images/'))
-);
+});
+
+gulp.task('js', () => {
+    gulp.src('./src/js/**/*.js') 
+  .pipe(babel({
+        presets: ['env']
+      }))
+      .pipe(gulp.dest('./dist/js/'))
+    });
